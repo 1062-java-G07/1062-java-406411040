8f19ca9a2a7232d61957175c5de1cadb212f60c4
allen0114302 <39294985+allen0114302@users.noreply.github.com>
2018-06-07 19:52:13 +0800

    Add files via upload

---
+++ Damage.java
@@ -0,0 +1,8 @@
+
+public class Damage extends BeadPanel{
+	 public static void main(String[] args) {
+	        damage damege = new damage();
+	        son.getMoney();
+	        System.out.println("son.money="+son.money);
+	    }
+}
